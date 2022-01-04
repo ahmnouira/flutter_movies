7d818bcb300f9fa43156c57285447309232ad0df
Ahmed Nouira <ahmnouira@gmail.com>
2022-01-04 01:48:12 +0100

    Add movie model

---
+++ lib/src/models/movie.dart
@@ -0,0 +1,20 @@
+class Movie {
+  late int id;
+  late String title;
+  late double voteAverage;
+  late String releaseDate;
+  late String overview;
+  late String posterPath;
+
+  Movie(this.id, this.title, this.voteAverage, this.releaseDate, this.overview,
+      this.posterPath);
+
+  Movie.fromJson(Map<String, dynamic> parsedJson) {
+    this.id = parsedJson['id'];
+    this.title = parsedJson['title'];
+    this.voteAverage = parsedJson['vote_average'] * 1.0;
+    this.releaseDate = parsedJson['release_date'];
+    this.overview = parsedJson['overview'];
+    this.posterPath = parsedJson['poster_path'];
+  }
+}
--- lib/src/services/movie_service.dart
+++ lib/src/services/movie_service.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/cupertino.dart';
-import 'package:http/http.dart' as http;
-import "dart:io"; 
+import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_movies/src/models/movie.dart';
+import 'package:http/http.dart' as http;
+import "dart:io";
 
 class MovieService {
   final String urlKey = "api_key=f9dba6179c34a9041a341f2c91214d0b";
@@ -9,12 +11,16 @@ class MovieService {
   final String urlUpcoming = "/upcoming?";
   final String urlLangauge = "&langauge=en-US";
 
-  Future<String> getUpcoming() async {
+  Future<List<Movie>> getUpcoming() async {
     final String upcoming = urlBase + urlUpcoming + urlKey + urlLangauge;
-    http.Response result  = await http.get(Uri.parse(upcoming)); 
-    if(result.statusCode == HttpStatus.ok) {
-      String responseBody = result.body; 
-      return responseBody;
+    http.Response result = await http.get(Uri.parse(upcoming));
+    if (result.statusCode == HttpStatus.ok) {
+      final jsonResponse = json.decode(result.body);
+      print("jsonReponse" + jsonResponse);
+      final movieMap = jsonResponse['results'];
+      print(movieMap is Map<String, dynamic>);
+      List<Movie> movies = movieMap.map((id) => Movie.fromJson(id));
+      return movies;
     } else {
       throw new ErrorDescription(result.statusCode.toString());
     }
